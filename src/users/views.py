ef9a9eeaaebd70f73e41649afe0bd13b3c171be6
emu <himtiaz@dohatec.com.bd>
2018-09-16 12:43:15 +0600

    static files added, Authentication UI added

--- src/users/views.py
+++ src/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
-  pass
+  return render(request, "users/login.html", {})
 
 def show(request, id=None):
   pass
