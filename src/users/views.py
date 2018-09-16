e734353ddd3694dab79fcefd938661a3881f768d
emu <himtiaz@dohatec.com.bd>
2018-09-16 17:29:58 +0600

    User creation form validation

--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
 def login(request):
   return render(request, "users/login.html", {})
 
@@ -10,7 +12,19 @@ def show(request, id=None):
   pass
 
 def register(request):
-  return render(request, "users/register.html", {})
+  if request.method == 'POST':
+    f = CustomUserCreationForm(request.POST)
+    if f.is_valid():
+      f.save()
+      messages.success(request, 'Account created successfully')
+      return redirect('users:register')
+
+  else:
+    f = CustomUserCreationForm()
+
+  errors = ' '.join([' '.join(x for x in l) for l in list(f.errors.values())])
+
+  return render(request, "users/register.html", {'form': f, 'errors': errors})
 
 def create(request):
   pass
