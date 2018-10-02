94c6921331395d67e0900f6dcee8ab480d221c60
emu <himtiaz@dohatec.com.bd>
2018-10-02 16:31:20 +0600

    Add tabs in Workboards

--- src/workboard/views.py
+++ src/workboard/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 def show(request, id=None):
 	workboard = get_object_or_404(Workboard, id=id)
+	workboards = Workboard.objects.filter(project=workboard.project)
 	context = {
-		'workboard': workboard
+		'workboard': workboard,
+		'workboards': workboards
 	}
 	return render(request, "workboards/show.html", context)
