d6d65a008e87f8d8ee4d4f9d4f2e20c644a9c2af
emu <himtiaz@dohatec.com.bd>
2018-10-04 16:06:00 +0600

    Added JS Tree on workboards

--- src/workboard/views.py
+++ src/workboard/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from .models import Workboard
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Workboard
+from analysis_types.models import AnalysisType
 
 
 # Create your views here.
 def show(request, id=None):
 	workboard = get_object_or_404(Workboard, id=id)
 	workboards = Workboard.objects.filter(project=workboard.project)
+	analysis_types = AnalysisType.objects.all()
 	context = {
 		'workboard': workboard,
-		'workboards': workboards
+		'workboards': workboards,
+		'analysis_types': analysis_types
 	}
 	return render(request, "workboards/show.html", context)
