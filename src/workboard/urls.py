09754dbbf880ff8cbf07538bbafeaab8c977dc54
emu <himtiaz@dohatec.com.bd>
2018-10-23 15:56:26 +0600

    Bubble Chart Api added

--- src/workboard/urls.py
+++ src/workboard/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import show, data_chart, data_table, create
+from .views import show, data_chart, data_table, create, data_bubble
 
 urlpatterns = [
 	url(r'^(?P<id>\d+)/$', show, name='show'),
 	url(r'^create/$', create, name='create'),
 	url(r'^(?P<id>\d+)/data_chart/$', data_chart, name='chart'),
+	url(r'^(?P<id>\d+)/data_bubble/$', data_bubble, name='bubble'),
 	url(r'^(?P<id>\d+)/data_table/$', data_table, name='table'),
 ]
--- src/workboard/views.py
+++ src/workboard/views.py
@@ -78,7 +78,7 @@ def data_chart(request, id=None):
 	engine = create_engine(dbCon.connection_string, echo=True)
 	cnx = engine.raw_connection()
 	collection_name = list(variables.keys())[0]
-	
+
 	yaxis = []
 	column_names = []
 	column_types = []
@@ -121,6 +121,71 @@ def data_chart(request, id=None):
 			'analysis_type': request.POST.get('type', None),
 			'columns': column_names,
 			'workboard': workboard
+		}
+
+	except Exception as e:
+		json_data = {
+			'analysis_type': request.POST.get('type', None),
+			'error': e.args[0],
+			'workboard': workboard
+		}
+
+	template = render_to_string('workboards/workboard_chart.html', json_data)
+	return HttpResponse(json.dumps(template), content_type='application/json')
+
+
+@csrf_exempt
+def data_bubble(request, id=None):
+	workboard = get_object_or_404(Workboard, id=id)
+	dbCon = ConnectedDatabase.objects.get(project=workboard.project)
+	variables = json.loads(request.POST.get('variables', None))
+	engine = create_engine(dbCon.connection_string, echo=True)
+	cnx = engine.raw_connection()
+	collection_name = list(variables.keys())[0]
+
+	column_names = []
+	yaxis = None
+	for item in variables[collection_name]:
+		column_names.append(item['name'])
+		if item['type'] == 'string':
+			xaxis = item['name']
+		elif yaxis == None and item['type'] == 'integer':
+			yaxis = item['name']
+		elif item['type'] == 'integer':
+			zaxis = item['name']
+
+	finalResult = []
+
+	try:
+		data = pd.read_sql(
+			'Select \"' + collection_name + "\".\"" + xaxis + '\" as x, \"' + collection_name + "\".\"" + yaxis \
+			+ '\" as y, \"' + collection_name + "\".\"" + zaxis + '\" as size FROM \"' + collection_name + "\"",
+			cnx)
+
+		datasum = data.groupby(['x']).sum()
+		datasum['index_col'] = range(0, len(datasum))
+		datamin = data.groupby(['x']).min()
+		datamin['index_col'] = range(0, len(datamin))
+		datamax = data.groupby(['x']).max()
+		datamax['index_col'] = range(0, len(datamax))
+		dataavg = data.groupby(['x']).mean()
+		dataavg['index_col'] = range(0, len(dataavg))
+
+		resultsumdump = json.JSONDecoder().decode(datasum.reset_index().to_json(orient="records"))
+		resultmindump = json.JSONDecoder().decode(datamin.reset_index().to_json(orient="records"))
+		resultmaxdump = json.JSONDecoder().decode(datamax.reset_index().to_json(orient="records"))
+		resultavgdump = json.JSONDecoder().decode(dataavg.reset_index().to_json(orient="records"))
+
+		finalResult.append(
+			{'sum': resultsumdump, 'avg': resultavgdump, 'min': resultmindump, 'max': resultmaxdump, 'var_name': xaxis})
+
+		json_data = {
+			'data': json.dumps(finalResult),
+			'analysis_type': request.POST.get('type', None),
+			'columns': column_names,
+			'workboard': workboard,
+			'yaxis': yaxis,
+			'zaxis': zaxis
 		}
 
 	except Exception as e:
